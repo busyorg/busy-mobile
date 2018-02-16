2d410561fde28a72fe3df3ca7a15ac4feae0fff8
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-02-16 15:32:18 +0100

    feat(post): render markdown

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -3,16 +3,38 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { ScrollView } from 'react-native';
 import styled from 'styled-components';
+import Markdown from 'react-native-simple-markdown';
+import styles from 'react-native-simple-markdown/styles';
 import { getPostById } from '../ducks';
 import Container from '../components/Container';
 import Header from '../components/PostFeed/components/Header';
-import Body from '../components/PostFeed/components/Body';
+
+const Body = styled.View`
+  padding: 16px;
+`;
 
 const Title = styled.Text`
   padding: 16px;
   font-size: 20px;
   font-weight: 500;
 `;
+
+const mdStyles = {
+  codeBlock: {
+    ...styles.codeBlock,
+    fontFamily: 'space-mono',
+    fontWeight: '400',
+  },
+  inlineCode: {
+    ...styles.inlineCode,
+    fontFamily: 'space-mono',
+  },
+  paragraph: {
+    ...styles.paragraph,
+    paddingTop: 8,
+    paddingBottom: 8,
+  },
+};
 
 class PostScreen extends React.Component {
   static navigationOptions = {
@@ -30,7 +52,9 @@ class PostScreen extends React.Component {
         <ScrollView>
           <Header author={post.author} created={post.created} />
           <Title>{post.title}</Title>
-          <Body>{post.body}</Body>
+          <Body>
+            <Markdown styles={mdStyles}>{post.body}</Markdown>
+          </Body>
         </ScrollView>
       </Container>
     );
